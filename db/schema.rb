606c3f77de04c5fc89555a6f79bbc3ff3085b2ec
ABStein <abs19stein@gmail.com>
2020-03-05 15:57:14 -0600

    update gems

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171112154506) do
+ActiveRecord::Schema.define(version: 20180215172922) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,11 @@ ActiveRecord::Schema.define(version: 20171112154506) do
     t.integer "year"
     t.string "make"
     t.string "model"
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
